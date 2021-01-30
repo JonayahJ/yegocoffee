fb7d4437e6dcbb4292d0937c48f8713c9a5e3c78
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-01-30 13:15:54 -0500

    added roasted and green coffee collections

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0e0bcd23e52c0e7d080f";
+/******/ 	var hotCurrentHash = "d7548c94fd879f07fdb8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
